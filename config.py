c3a46a0c4e7de6dbed3d3a74fb626ccbe354cf0f
Ankit Vani <a@nevitus.org>
2016-07-19 19:38:40 -0400

    more reasonable defaults

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ flags = tf.flags
 
 # command-line config
 flags.DEFINE_string ("data_path",          "/home/ankit/devel/data/MIMIC3pk", "Data path")
-flags.DEFINE_string ("save_file",          "savemodel.dat", "Save file")
-flags.DEFINE_string ("load_file",          "loadmodel.dat", "File to load model from")
+flags.DEFINE_string ("save_file",          "model.dat", "Save file")
+flags.DEFINE_string ("load_file",          "", "File to load model from")
 flags.DEFINE_float  ("init_scale",         0.05, "Variable initialization scale")
 flags.DEFINE_float  ("learning_rate",      1e-3, "ADAM learning rate")
 flags.DEFINE_float  ("max_grad_norm",      5,    "Gradient clipping")
@@ -17,7 +17,7 @@ flags.DEFINE_integer("max_epoch",          6,    "Maximum number of epochs to ru
 flags.DEFINE_float  ("keep_prob",          0.5,  "Dropout keep probability")
 flags.DEFINE_integer("batch_size",         20,   "Batch size")
 flags.DEFINE_integer("print_every",        200,  "Print every these many steps")
-flags.DEFINE_integer("save_every",         500,  "Save every these many steps") # TODO change
+flags.DEFINE_integer("save_every",         5000,  "Save every these many steps")
 flags.DEFINE_bool   ("pretrained_emb",     True, "Use pretrained embeddings")
 flags.DEFINE_bool   ("conditional",        True, "Use a conditional language model")
 
@@ -29,6 +29,7 @@ class Config(object):
     mimic_embeddings = {'gender': 1, 'has_dod': 1, 'has_icu_stay': 1,
                         'admission_type': 3, 'diagnoses': 40, 'procedures': 40,
                         'labs': 40, 'prescriptions': 40}
+
 
     def __init__(self):
         for k, v in flags.FLAGS.__dict__['__flags'].items():
