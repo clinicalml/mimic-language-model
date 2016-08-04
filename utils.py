8373f9c9c68320bbd8badcf37e1b216362abf0f7
Ankit Vani <a@nevitus.org>
2016-08-04 17:59:59 -0400

    plot sparsity values

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import itertools
 import random
 
+import matplotlib.pyplot as plt
+
 import tensorflow as tf
 import numpy as np
 
@@ -43,5 +45,40 @@ def l1_norm(tensor):
     return tf.reduce_sum(tf.abs(tensor))
 
 
+def inspect_feature(feat, embedding, config, vocab, verbose=False, graph=True):
+    print '\n\n' + feat + '\n'
+    vocab_size, dims = embedding.shape
+    if verbose and vocab_size < 10 and dims < 10:
+        for i in xrange(vocab_size):
+            try:
+                toprint = vocab.aux_list[feat][i]
+            except:
+                toprint = i
+            print str(toprint).ljust(20), embedding[i]
+    if graph and feat in config.var_len_features:
+        embedding = sorted(np.abs(embedding.flatten()).tolist(), reverse=True)
+        print len(embedding)
+        plt.figure(figsize=(14,10))
+        plt.plot(embedding)
+        plt.axis([0, len(embedding)-1, 0.0, embedding[0]])
+        plt.title('Sparsity pattern for '+feat)
+        plt.ylabel('Absolute values')
+        plt.show()
+
+
 def inspect(session, m, config, vocab, saver):
-    pass #TODO
+    with tf.device("/cpu:0") and tf.variable_scope("model", reuse=True):
+        for i, (feat, dims) in enumerate(config.mimic_embeddings.items()):
+            if dims <= 0: continue
+            try:
+                vocab_aux = len(vocab.aux_list[feat])
+            except KeyError:
+                vocab_aux = 2 # binary
+            with tf.device("/cpu:0"):
+                vocab_dims = vocab_aux
+                if feat in config.var_len_features:
+                    vocab_dims -= 1
+                embedding = tf.get_variable("struct_embedding."+feat, [vocab_dims,
+                                                                    config.mimic_embeddings[feat]],
+                                           initializer=tf.truncated_normal_initializer(stddev=0.1))
+                inspect_feature(feat, embedding.eval(), config, vocab)
